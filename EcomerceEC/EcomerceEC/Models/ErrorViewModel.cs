fe5b4ada4a210bdcebac36571a7a73907905e4ff
Juan Salazar <juanzjck1996@gmail.com>
2020-01-18 00:46:54 -0500

    Create project

---
+++ EcomerceEC/EcomerceEC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcomerceEC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
